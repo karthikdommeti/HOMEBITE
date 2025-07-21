75c13d9a910d4b1bae0c1fc8cd650b6f71486071
Karthik Dommeti <145791133+karthikdommeti@users.noreply.github.com>
2025-07-21 15:32:36 +0530

    Update Dish.js

--- backend/models/Dish.js
+++ backend/models/Dish.js
@@ -1,14 +1,13 @@
-
 const mongoose = require("mongoose");
 
 const DishSchema = new mongoose.Schema({
-    vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    dishName: String,
-    dishPhoto: String,
-    price: Number,
-    contactNumber: String,
-    type: { type: String, enum: ['veg', 'nonveg'], required: true },
-    likes: { type: Number, default: 0 }
+  vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  dishName: { type: String, required: true },
+  dishPhoto: { type: String, required: true },
+  price: { type: Number, required: true },
+  contactNumber: { type: String, required: true },
+  type: { type: String, enum: ['veg', 'nonveg'], required: true },
+  likes: { type: Number, default: 0 }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Dish", DishSchema);
